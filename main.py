03d8a5094e98c66ca36729d09109fdf72e08df24
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-09-10 11:28:25 +0200

    .

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ def get_site_data(varname: str, obsnetwork: str, model: str, time_config: dict) 
     )
     result = []
     for md in map_data:
+        # Loop through station entries extracting their characteristics.
         extracted_data = {
             k: v
             for k, v in md.items()
@@ -64,7 +65,7 @@ def get_site_data(varname: str, obsnetwork: str, model: str, time_config: dict) 
         }
 
         # Skip stations for which stats timeseries or year/season combination does
-        # not exist.
+        # not exist by not appending them to results array.
         if not stats in md:
             continue
         if not f"{year}-{season}" in md[stats]:
@@ -78,8 +79,55 @@ def get_site_data(varname: str, obsnetwork: str, model: str, time_config: dict) 
 
 sites = get_site_data("concNno", "EBAS-m", "v5.3", time_config)
 
+# Show full record for the first entry:
 print(sites[0])
 
-#python main.py 
-#Fetching data from 'https://api.aeroval-test.met.no/api/0.2.1/map/rv5_series/DSemep/EBAS-m/concNno/Surface/v5.3/concNno/2022?data_path=davids'
-#{'station_name': 'Aliartos', 'latitude': 38.366667, 'longitude': 23.083333, 'altitude': 110.0, 'region': ['Greece']}
+# List names for all entries:
+for name in [s["station_name"] for s in sites]:
+    print(name)
+
+# OUTPUT:
+# python main.py 
+# Fetching data from 'https://api.aeroval-test.met.no/api/0.2.1/map/rv5_series/DSemep/EBAS-m/concNno/Surface/v5.3/concNno/2022?data_path=davids'
+# {'station_name': 'Aliartos', 'latitude': 38.366667, 'longitude': 23.083333, 'altitude': 110.0, 'region': ['Greece']}
+# Aliartos
+# Anholt
+# Aston Hill
+# Barcarrota
+# Birmingham Air Quality Site (BAQS)
+# Bush
+# Cabauw Wielsekade
+# Cabo de Creus
+# Charlton Mackrell
+# Chilbolton Observatory
+# De Zilk
+# Diabla Gora
+# Doñana
+# Eibergen
+# Els Torms
+# High Muffles
+# Houtem
+# Hyytiälä
+# Ispra
+# Keldsnor
+# Kollumerwaard
+# Ladybower Res.
+# Lullington Heath
+# Mahón
+# Moerkerke
+# Narberth
+# Neuglobsow
+# Niembro
+# Oulanka
+# Payerne
+# Risø
+# SIRTA Atmospheric Research Observatory
+# St. Osyth
+# Ulborg
+# Utö
+# Virolahti III
+# Vredepeel
+# Waldhof
+# Wicken Fen
+# Yarner Wood
+# Zingst
