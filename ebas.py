25715e5007d70c216f654ad33ac4d29f4c9814f2
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-10-01 10:19:25 +0200

    Some changes

--- ebas.py
+++ ebas.py
@@ -23,27 +23,32 @@ FOLDER_TO_READ = pathlib.Path(
 def ebas_components_for_aerocom_variable(aerocom_variable: str):
     return EbasVarInfo(aerocom_variable)["component"]
 
+def get_datasets(var_name: str, site: str, matrix: str, start_time: datetime.datetime, end_time: datetime.datetime):
+    con = sqlite3.connect(str(EBAS_FILE_INDEX))
+    con.row_factory = sqlite3.Row
 
-# print(ebas_components_for_aerocom_variable("conco3"))
-VAR_NAME = "concso4"
-SITE = "AT0002R"
+    cur = con.cursor()
+    
+    comps = ebas_components_for_aerocom_variable(var_name)
 
-con = sqlite3.connect(str(EBAS_FILE_INDEX))
-cur = con.cursor()
-
-all = []
-
-for comp in ebas_components_for_aerocom_variable(VAR_NAME):
     cur.execute(
-        """
-        SELECT station_code,comp_name,unit, filename FROM variable
+        f"""
+        SELECT DISTINCT filename,station_code,comp_name,unit,matrix
+        
+        FROM 
+            variable
         WHERE
-            station_code = ? AND comp_name = ?
+            station_code = ?
+            AND comp_name IN ({",".join('?'*len(comps))}) 
+            AND matrix = ?
+            AND first_start >= ?
+            AND last_end <= ?
+        GROUP BY
+            filename
         """,
-        (SITE, comp),
+        (site, *comps, matrix, start_time, end_time),
     )
-    all.extend(cur.fetchall())
-
+    return cur.fetchall()
 
 def read_ebas_data(file_name: str) -> pd.DataFrame:
     """
@@ -62,7 +67,7 @@ def read_ebas_data(file_name: str) -> pd.DataFrame:
         ebas_var = None
         for x in ebas_components_for_aerocom_variable(VAR_NAME):
             for y in ts.variables():
-                if f"#{x}#" in y:
+                if f"{MATRIX}#{x}#" in y:
                     print(f"Using variable name '{y}'")
                     ebas_var = y
 
@@ -87,148 +92,24 @@ def read_ebas_data(file_name: str) -> pd.DataFrame:
 
     return df
 
-
-def get_component_info(file_names: list[str], aerocom_var: str | None = None):
-    """
-    Returns a dictionary of information about components provided by
-    ebas files. The dictionary has a single entry per file path with a sub dict
-    containing a list of pairwise comp_names and units.
-
-    If aerocom_var is set, only components that apply to that aerocom variable
-    name will be listed.
-
-    """
-    con = sqlite3.connect(str(EBAS_FILE_INDEX))
-    cur = con.cursor()
-
-    files = set(file_names)
-    result = {}
-    cur.execute(
-        """
-        SELECT comp_name,unit,filename FROM variable
-        """
-    )
-
-    for x in cur.fetchall():
-        if x[2] in files:
-            if x[2] not in result:
-                result[x[2]] = {}
-            if "component" not in result[x[2]]:
-                result[x[2]]["component"] = []
-            if "unit" not in result[x[2]]:
-                result[x[2]]["unit"] = []
-
-            if (
-                x[0] in ebas_components_for_aerocom_variable(aerocom_var)
-                or aerocom_var is None
-            ):
-                result[x[2]]["component"].append(x[0])
-                result[x[2]]["unit"].append(x[1])
-
-    return result
-
-def get_most_recent_file(files: list[str]) -> tuple[str, str]:
-    """
-    For a list of ebas files, returns the file with the most recent revision string.
-    """
-    rev, file = None, None
-    engines = pyaro.list_timeseries_engines()
-    for f in files:
-        with engines["nilupmfebas"].open(
-            f, filters=[pyaro.timeseries.filters.get("stations", include=[SITE])]
-        ) as ts:
-            revision = datetime.datetime.strptime(ts.metadata()["revision"], "%y%m%d%H%M%S")
-
-            if rev is None or rev < revision:
-                rev = revision
-                file = f
-    
-    return rev, file
-
-
-
 if __name__ == "__main__":
-    print(f"{len(all)} files found")
+    VAR_NAME = "concso4"
+    SITE = "AT0002R"
+    MATRIX = "aerosol"
+    START_TIME = datetime.datetime(2010, 1, 1, 0, 0, 0)
+    END_TIME = datetime.datetime(2011, 12, 31, 23, 59, 59)
 
-    # Print available files and components for the selected station
-    # and aerocom variable name.
-    info = get_component_info([x[3] for x in all], VAR_NAME)
-    for f in all:
-        print(f" - {f[3]}")
-        for c, u in zip(info[f[3]]["component"], info[f[3]]["unit"]):
-            print(f"   - {c} ({u})")
+    files = get_datasets(VAR_NAME, SITE, MATRIX, START_TIME, END_TIME)
 
-    # Print the revision date and file of the most recent file.
-    rev, file = get_most_recent_file([f"{FOLDER_TO_READ}/{f[3]}" for f in all])
-    print(f"Most recent file: '{file}' ({rev})")
+    print(f"{len(files)} files found")
 
     # Example access of data for a file
-    data = read_ebas_data(
-        f"{FOLDER_TO_READ}/AT0002R.19870101070000.20110303000000.wet_only_sampler..precip.1y.1d.AT01L_wados_02.AT01L_IC_a.lev2.nas"
-    )
-    print(f"Column names: {data.columns}")
-    print(data)
+    data = {}
+    for f in [f"{FOLDER_TO_READ}/{x['filename']}" for x in files]:
+        print(f"Reading file '{f}'...")
+        data[f] = read_ebas_data(f)
 
-    # Example output:
-    # 206 files found
-    # ...
-    #     - AT0002R.20080101080000.20110303000000.wet_only_sampler..precip.1y.1d.AT01L_wados_02..lev2.nas
-    #   - sulphate_corrected (mg S/l)
-    #   - sulphate_corrected (mg/l)
-    #   - sulphate_total (mg S/l)
-    #   - sulphate_total (mg/l)
-    # - AT0002R.20090101080000.20110303000000.wet_only_sampler..precip.1y.1d.AT01L_wados_02..lev2.nas
-    #   - sulphate_corrected (mg S/l)
-    #   - sulphate_corrected (mg/l)
-    #   - sulphate_total (mg S/l)
-    #   - sulphate_total (mg/l)
-    # - AT0002R.20090101080000.20110303000000.wet_only_sampler..precip.1y.1d.AT01L_wados_02..lev2.nas
-    #   - sulphate_corrected (mg S/l)
-    #   - sulphate_corrected (mg/l)
-    #   - sulphate_total (mg S/l)
-    #   - sulphate_total (mg/l)
-    # - AT0002R.20100101000000.20140227000000.filter_3pack..aerosol.1y.1d.AT01L_f3p_02..lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # - AT0002R.20100101000000.20140227000000.filter_3pack..aerosol.1y.1d.AT01L_f3p_02..lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # - AT0002R.20110101000000.20140227000000.filter_3pack..aerosol.6d.1d.AT01L_f3p_02..lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # - AT0002R.20110101000000.20140227000000.filter_3pack..aerosol.6d.1d.AT01L_f3p_02..lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # - AT0002R.20220712000000.20240530000000.high_vol_sampler..pm25.10d.1d.AT03L_hvs_at0002.FR18L_tracers_IMP2022.lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # - AT0002R.20220712000000.20240530000000.high_vol_sampler..pm25.10d.1d.AT03L_hvs_at0002.FR18L_tracers_IMP2022.lev2.nas
-    #   - sulphate_total (ug S/m3)
-    #   - sulphate_total (ug/m3)
-    # Using variable name 'precip#sulphate_corrected#mg S/l'
-    # Using variable name 'precip#sulphate_corrected#mg/l'
-    # Using variable name 'precip#sulphate_total#mg S/l'
-    # Using variable name 'precip#sulphate_total#mg/l'
-    # Column names: Index(['start_time', 'end_time', 'latitude', 'longitude', 'altitude',
-    #       'station', 'flag', 'value_precip#sulphate_corrected#mg S/l',
-    #       'stdev_precip#sulphate_corrected#mg S/l',
-    #       'value_precip#sulphate_corrected#mg/l',
-    #       'stdev_precip#sulphate_corrected#mg/l',
-    #       'value_precip#sulphate_total#mg S/l',
-    #       'stdev_precip#sulphate_total#mg S/l',
-    #       'value_precip#sulphate_total#mg/l', 'stdev_precip#sulphate_total#mg/l'],
-    #      dtype='object')
-    #             start_time            end_time  ...  value_precip#sulphate_total#mg/l  stdev_precip#sulphate_total#mg/l
-    # 0   1987-01-01 07:00:00 1987-01-02 07:00:00  ...                               1.9                               NaN
-    # 1   1987-01-02 07:00:00 1987-01-03 07:00:00  ...                               NaN                               NaN
-    # 2   1987-01-03 07:00:00 1987-01-04 07:00:00  ...                               NaN                               NaN
-    # 3   1987-01-04 07:00:00 1987-01-05 07:00:00  ...                               NaN                               NaN
-    # 4   1987-01-05 07:00:00 1987-01-06 07:00:00  ...                               NaN                               NaN
-    # ..                  ...                 ...  ...                               ...                               ...
-    # 360 1987-12-27 07:00:00 1987-12-28 07:00:00  ...                               NaN                               NaN
-    # 361 1987-12-28 07:00:00 1987-12-29 07:00:00  ...                               4.2                               NaN
-    # 362 1987-12-29 07:00:00 1987-12-30 07:00:00  ...                               NaN                               NaN
-    # 363 1987-12-30 07:00:00 1987-12-31 07:00:00  ...                               NaN                               NaN
-    # 364 1987-12-31 07:00:00 1988-01-01 07:00:00  ...                               NaN                               NaN
-    #
-    # [365 rows x 15 columns]
+    # data is now a dictionary mapping from the source file_path to the corresponding loaded data
+    # frame.
+    print(list(data.values())[0])
+    print(list(data.values())[0].columns)
--- ebas2.py
+++ ebas2.py
@@ -11,10 +11,10 @@ def to_dataframe(ungridded: pya.ungriddeddata.UngriddedData) -> pd.DataFrame:
    
     # Spatio-temporal information
     df["time"] = ungridded._data[:, ungridded._TIMEINDEX].astype("datetime64[s]")
-    df["var_name"] = [list(ungridded.metadata[x]["var_info"].values())[0]["var_name"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
-    df["latitude"] = ungridded._data[:, ungridded._LATINDEX]
-    df["longitude"] = ungridded._data[:, ungridded._LONINDEX]
-    df["altitude"] = ungridded._data[:, ungridded._ALTITUDEINDEX]
+    #df["var_name"] = [list(ungridded.metadata[x]["var_info"].values())[0]["var_name"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["latitude"] = ungridded._data[:, ungridded._LATINDEX]
+    #df["longitude"] = ungridded._data[:, ungridded._LONINDEX]
+    #df["altitude"] = ungridded._data[:, ungridded._ALTITUDEINDEX]
     
     # Actual data values.
     df["value"] = ungridded._data[:, ungridded._DATAINDEX]
@@ -26,11 +26,11 @@ def to_dataframe(ungridded: pya.ungriddeddata.UngriddedData) -> pd.DataFrame:
     # {'name': 'ozone', 'units': 'nmol mol-1', 'matrix': ['air'], 'statistics': None, 'ts_type': 'hourly', 'converted_from_units': 'ug/m3', 'units_conv_fac': 0.49260573482126196, 'computed': True, 'var_name': 'vmro3max', 'component': ['ozone'], 'instrument': None, 'requires': None, 'scale_factor': 1}
 
     # Add metadata.
-    df["station_id"] = [ungridded.metadata[x]["station_id"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
-    df["station_name"] = [ungridded.metadata[x]["station_name"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
-    df["country_code"] = [ungridded.metadata[x]["country_code"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
-    df["country"] = [ungridded.metadata[x]["country"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
-    df["ts_type"] = [ungridded.metadata[x]["ts_type"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["station_id"] = [ungridded.metadata[x]["station_id"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["station_name"] = [ungridded.metadata[x]["station_name"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["country_code"] = [ungridded.metadata[x]["country_code"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["country"] = [ungridded.metadata[x]["country"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
+    #df["ts_type"] = [ungridded.metadata[x]["ts_type"] for x in ungridded._data[:, ungridded._METADATAKEYINDEX]]
     #
 
     # Available metadata keys are the following: 
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     
     # Filtering for station here is more time and memory efficient.
     # Can filter on any value in the metadata dict (See previous comment)
-    data = data.filter_by_meta(ts_type = "hourly", station_id = "AM0001R")
+    data = data.filter_by_meta(ts_type = "hourly", station_id = "NO0001R")
 
     df = to_dataframe(data)
 
@@ -59,13 +59,9 @@ if __name__ == "__main__":
     # Filter for date range.
     start = datetime.datetime(2010, 3, 14, 12, 0, 0)
     end = datetime.datetime(2010, 4, 14, 12, 0, 0)
-    df = df[np.logical_and(df["time"] >= start, df["time"] <= end)]
+    #df = df[np.logical_and(df["time"] >= start, df["time"] <= end)]
 
     # Ensure timeseries is sorted (may not be necessary)
     df.sort_values("time", ascending=True)
-
-    # Select station
-    df = df[df["station_id"] == "AM0001R"]
-    #df = df[df["station_name"] == "Amberd"]
 
     print(df)
