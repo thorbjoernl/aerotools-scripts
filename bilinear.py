97985df373552120c0d6cf590f86624c794ad5c1
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-10-16 11:51:30 +0200

    .

--- bilinear.py
+++ bilinear.py
@@ -4,19 +4,21 @@ import iris.cube
 import pyaerocom as pya
 
 if __name__ == "__main__":
-    mod_reader = pya.io.mscw_ctm.reader.ReadMscwCtm(
-        "EMEP",
-        data_dir="/lustre/storeB/project/fou/kl/emep/ModelRuns/2024_REPORTING/TRENDS/2020",
-    )
-
-    mod_data: pya.griddeddata.GriddedData = mod_reader.read_var("concnh3", ts_type="daily")
-
     obs_reader = pya.io.ReadUngridded("EBASMC")
-
     obs_data: pya.ungriddeddata.UngriddedData = obs_reader.read(
-        vars_to_retrieve="concnh3",
+        vars_to_retrieve="vmro3",
         #ts_type="daily"
     )
+
+    w = min(obs_data.longitude) ; e = max(obs_data.longitude) ; s = min(obs_data.latitude) ; n = max(obs_data.latitude)
+    box = pya.utils.BoundingBox(west=w, east=e, south=s, north=n)
+
+    mod_reader = pya.io.mscw_ctm.reader.ReadMscwCtm(
+        "EMEP",
+        data_dir="/lustre/storeB/project/fou/kl/emep/ModelRuns/2024TESTS/CAO4_series/EMEP0302c_n8rv5_4DS_CAO4nvdC_emis2015.2019", 
+    )
+    
+    mod_data: pya.griddeddata.GriddedData = mod_reader.read_var("vmro3", ts_type="hourly", bounding_box = box)
 
     coord_pairs = obs_data._get_stat_coords()[1]
     lat, lon = list(zip(*coord_pairs))
