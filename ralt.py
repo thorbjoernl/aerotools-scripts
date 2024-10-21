8142ad961d41485016d7deb2a56b16a8c5be39c1
thorbjoernl <51087536+thorbjoernl@users.noreply.github.com>
2024-10-21 16:02:42 +0200

    docs: Comment assumptions and limitations

--- ralt.py
+++ ralt.py
@@ -1,4 +1,16 @@
 from utils.relalt import get_relative_altitude
 
+# Uses default TOPO folder at /lustre/storeB/project/aerocom/aerocom1/AEROCOM_OBSDATA/GTOPO30/nc
+# Can override using the topodir argument, but currently this will only work for 
+# GTOPO30, or very similarly structured datasets.
+#
+# A couple of notes / and limitations:
+# - Distance is calculated based on the haversine formula.
+# - Minimum altitude is currently capped at 0.
+# - All elevation / distance units are in meters.
+# - Some areas may not have data available (eg. in the middle of the ocean) and 
+# will raise a ValueError.
+# - The topo dataset is split into multiple files, and currently only the file which 
+# encompasses the lat/lon point is used for calculation. May give unexpected values along boundaries.
 print(f"Relative height of Galdhøpiggen: {get_relative_altitude(61.636471, 8.312443, altitude=2468):.2f} meters")
 
